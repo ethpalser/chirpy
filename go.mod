eb246df5ae3c5db469ed459c2a3bd4b94f0784ff
ethpalser <eth.palser@gmail.com>
2024-04-29 21:05:29 -0400

    setup basic server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module chirpy.com/m
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// Create a multiplexer that can handle HTTP requests for a server at its endpoints
+	mux := http.NewServeMux()
+	// Update the multiplexer to accept CORS data
+	corsMux := middlewareCors(mux)
+	// Setup a server that uses the new multiplexer
+	server := http.Server{Addr: "localhost:8080", Handler: corsMux}
+	server.ListenAndServe()
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
