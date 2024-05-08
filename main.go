98fd365fd2f874d15fd55a2af6990b0a8b9cb340
ethpalser <eth.palser@gmail.com>
2024-05-08 17:50:23 -0400

    added hits since startup

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	// Create a multiplexer that can handle HTTP requests for a server at its endpoints
 	mux := http.NewServeMux()
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	apiCfg := apiConfig{}
 	// Creates a Handler that handles HTTP requests at the path and returns system files
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(handler))
 	mux.HandleFunc("/healthz", health)
+	mux.HandleFunc("/metrics", apiCfg.hits)
+	mux.HandleFunc("/reset", apiCfg.reset)
 	// Update the multiplexer to accept CORS data
 	corsMux := middlewareCors(mux)
 	// Setup a server that uses the new multiplexer
@@ -34,4 +39,24 @@ func health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits += 1
+	return next
+}
+
+func (cfg *apiConfig) hits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + fmt.Sprint(cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
 }
